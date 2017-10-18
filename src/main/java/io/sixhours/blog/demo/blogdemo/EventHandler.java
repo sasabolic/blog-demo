6392506893baaf226795f09f6bff62d238df9d9a
Sasa Bolic <sasa.bolic@sixhours.io>
2017-10-18 22:15:23 +0200

    Organize imports

--- src/main/java/io/sixhours/blog/demo/blogdemo/EventHandler.java
+++ src/main/java/io/sixhours/blog/demo/blogdemo/EventHandler.java
@@ -1,11 +1,5 @@
 package io.sixhours.blog.demo.blogdemo;
 
-import com.twitter.bijection.Injection;
-import com.twitter.bijection.avro.GenericAvroCodecs;
-import org.apache.avro.Schema;
-import org.apache.avro.generic.GenericData;
-import org.apache.avro.generic.GenericRecord;
-
 /**
  * The abstract class of handler used in a chain.
  *
