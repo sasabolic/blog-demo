d20af04384eee2a267565d8cb9efd15d38d5014b
Sasa Bolic <sasa.bolic@sixhours.io>
2017-10-18 15:11:55 +0200

    Add integration test for BlogPost

---
+++ src/test/java/io/sixhours/blog/demo/blogdemo/BlogPostInteg.java
@@ -0,0 +1,19 @@
+package io.sixhours.blog.demo.blogdemo;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BlogPostInteg {
+
+    private BlogPost blogPost;
+
+    @Before
+    public void setUp() {
+        blogPost = new BlogPost(new EventService());
+    }
+
+    @Test
+    public void test() {
+        blogPost.process(new CreateBlogPostCommand("Title", "Body", "Author"));
+    }
+}
