e1d14f294eb9d94185641dc3a3aafeb9a01ddd4d
Sasa Bolic <sasa.bolic@sixhours.io>
2017-10-28 17:53:46 +0200

    Initialize blog-demo-query project

---
+++ blog-demo-query/src/test/java/io/sixhours/blog/demo/blogdemoquery/BlogDemoQueryApplicationTests.java
@@ -0,0 +1,16 @@
+package io.sixhours.blog.demo.blogdemoquery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlogDemoQueryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
