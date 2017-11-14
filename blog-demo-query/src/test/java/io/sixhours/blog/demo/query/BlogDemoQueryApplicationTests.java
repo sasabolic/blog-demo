3a77ba1570481e8d13110e35b92e51053b5955d3
Sasa Bolic <sasa.bolic@sixhours.io>
2017-11-14 17:30:07 +0100

    Remove spring tests & remove consumer logic from spring boot application starter

--- blog-demo-query/src/test/java/io/sixhours/blog/demo/query/BlogDemoQueryApplicationTests.java
+++ blog-demo-query/src/test/java/io/sixhours/blog/demo/query/BlogDemoQueryApplicationTests.java
@@ -1,15 +1,15 @@
-package io.sixhours.blog.demo.command.blogdemoquery;
+package io.sixhours.blog.demo.query;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class BlogDemoQueryApplicationTests {
 
-	@Test
+//	@Test
 	public void contextLoads() {
 	}
 
