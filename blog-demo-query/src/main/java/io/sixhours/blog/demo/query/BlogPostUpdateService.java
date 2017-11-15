c2b7332ec68b3cd73d82c1ee198eacd5c3605173
Sasa Bolic <sasa.bolic@sixhours.io>
2017-11-15 17:59:38 +0100

    Add BlogPostUpdateService

---
+++ blog-demo-query/src/main/java/io/sixhours/blog/demo/query/BlogPostUpdateService.java
@@ -0,0 +1,23 @@
+package io.sixhours.blog.demo.query;
+
+import io.sixhours.blog.demo.common.BlogPostCreated;
+import io.sixhours.blog.demo.common.BlogPostDeleted;
+import io.sixhours.blog.demo.common.BlogPostUpdated;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BlogPostUpdateService {
+    private static final Logger log = LoggerFactory.getLogger(BlogPostUpdateService.class);
+
+    public void create(BlogPostCreated event) {
+
+    }
+
+    public void update(BlogPostUpdated event) {
+
+    }
+
+    public void delete(BlogPostDeleted event) {
+
+    }
+}
