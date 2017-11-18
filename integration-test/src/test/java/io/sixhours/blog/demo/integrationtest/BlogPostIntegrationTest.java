7ad864b81a40a16b03848f1cdd20fd525790a433
Sasa Bolic <sasa.bolic@sixhours.io>
2017-11-18 12:53:14 +0100

    Change the way docker-compose files are loaded into DockerComposeRule

--- integration-test/src/test/java/io/sixhours/blog/demo/integrationtest/BlogPostIntegrationTest.java
+++ integration-test/src/test/java/io/sixhours/blog/demo/integrationtest/BlogPostIntegrationTest.java
@@ -1,20 +1,31 @@
 package io.sixhours.blog.demo.integrationtest;
 
 import com.palantir.docker.compose.DockerComposeRule;
+import com.palantir.docker.compose.configuration.DockerComposeFiles;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import io.sixhours.blog.demo.command.BlogPost;
+import org.junit.Before;
 import org.junit.ClassRule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Collections;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {"classpath:features"},
                     format = {"pretty", "html:build/reports/cucumber-report"},
                     glue = {"io.sixhours.blog.demo.integrationtest.steps"})
-public class BlogPostITest {
+public class BlogPostIntegrationTest {
+
+    static File file = new File(BlogPostIntegrationTest.class.getClassLoader().getResource("docker-compose.yml").getFile());
 
     @ClassRule
     public static DockerComposeRule docker = DockerComposeRule.builder()
-            .file("docker-compose.yml")
+            .files(new DockerComposeFiles(Collections.singletonList(file)))
             .build();
 
 }
