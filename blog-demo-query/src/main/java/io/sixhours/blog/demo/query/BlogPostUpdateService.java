99268b6c45f7dcb781b25c7ab493e1693ec826a9
Sasa Bolic <sasa.bolic@sixhours.io>
2017-11-18 13:12:21 +0100

    Add logic for updating elasticsearch index

--- blog-demo-query/src/main/java/io/sixhours/blog/demo/query/BlogPostUpdateService.java
+++ blog-demo-query/src/main/java/io/sixhours/blog/demo/query/BlogPostUpdateService.java
@@ -3,21 +3,103 @@ package io.sixhours.blog.demo.query;
 import io.sixhours.blog.demo.common.BlogPostCreated;
 import io.sixhours.blog.demo.common.BlogPostDeleted;
 import io.sixhours.blog.demo.common.BlogPostUpdated;
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.DocWriteResponse;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.IOException;
 
 public class BlogPostUpdateService {
     private static final Logger log = LoggerFactory.getLogger(BlogPostUpdateService.class);
 
+    private RestHighLevelClient client;
+
+
+    public BlogPostUpdateService() {
+        init();
+    }
+
+    @PostConstruct
+    public void init() {
+        client = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("localhost", 9200, "http")));
+    }
+
     public void create(BlogPostCreated event) {
+        log.info("Creating new blog post... '{}'", event.getAggregateId());
+
+        IndexRequest request = new IndexRequest("posts", "doc", event.getAggregateId().toString())
+                .source("title", event.getTitle(),
+                        "body", event.getBody(),
+                        "author", event.getAuthor(),
+                        "dateCreated", event.getDateCreated());
+
+        try {
+            IndexResponse response = client.index(request);
+            log.info("IndexResponse: {}", response);
+        } catch (Exception e) {
+            log.error("IndexResponse: Error", e);
+        }
 
     }
 
     public void update(BlogPostUpdated event) {
+        log.info("Updating blog post... '{}'", event.getAggregateId());
+
+        UpdateRequest request = new UpdateRequest("posts", "doc", event.getAggregateId().toString())
+                .doc("title", event.getTitle(),
+                        "body", event.getBody(),
+                        "author", event.getAuthor());
+
+        try {
+            log.info("BEGIN - SALE SALE SALE SALE SALE");
+            UpdateResponse response = client.update(request);
+            log.info("END - SALE SALE SALE SALE SALE");
+            log.info("UpdateResponse: {}", response);
+        } catch (Exception e) {
+            log.error("UpdateResponse: Error", e);
+        }
+
 
     }
 
     public void delete(BlogPostDeleted event) {
+        log.info("Deleting blog post... '{}", event.getAggregateId());
 
+        DeleteRequest request = new DeleteRequest("posts", "doc", event.getAggregateId().toString());
+
+        try {
+            DeleteResponse response = client.delete(request);
+            if (response.getResult() == DocWriteResponse.Result.NOT_FOUND) {
+                log.warn("Document '{}' marked as DELETED was not found", event.getAggregateId());
+            }
+
+            log.info("DeleteResponse: {}", response);
+        } catch (Exception e) {
+            log.error("DeleteResponse: Error", e);
+        }
+    }
+
+    @PreDestroy
+    public void destroy() {
+        if (client != null) {
+            try {
+                client.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- blog-demo-query/src/test/java/io/sixhours/blog/demo/query/BlogPostUpdateServiceTest.java
+++ blog-demo-query/src/test/java/io/sixhours/blog/demo/query/BlogPostUpdateServiceTest.java
@@ -1,4 +1,44 @@
 package io.sixhours.blog.demo.query;
 
+import io.sixhours.blog.demo.common.BlogPostCreated;
+import io.sixhours.blog.demo.common.BlogPostDeleted;
+import io.sixhours.blog.demo.common.BlogPostUpdated;
+import org.junit.Test;
+
+import java.time.Instant;
+import java.util.UUID;
+
 public class BlogPostUpdateServiceTest {
+
+    BlogPostUpdateService updateService = new BlogPostUpdateService();
+
+    @Test
+    public void whenCreateThenInvokeClientIndex() {
+        BlogPostCreated blogPostCreated = new BlogPostCreated(UUID.randomUUID(), "Title", "Body", "Author", Instant.now());
+
+        updateService.create(blogPostCreated);
+
+    }
+
+    @Test
+    public void whenUpdateThenInvokeClientIndex() {
+        BlogPostCreated blogPostCreated = new BlogPostCreated(UUID.randomUUID(), "Title", "Body", "Author", Instant.now());
+        BlogPostUpdated blogPostUpdated = new BlogPostUpdated(blogPostCreated.getAggregateId(), "TitleUpdated", "BodyUpdated", "AuthorUpdated");
+
+        updateService.create(blogPostCreated);
+
+        updateService.update(blogPostUpdated);
+
+    }
+
+    @Test
+    public void whenDeleteThenInvokeClientDelete() {
+        BlogPostCreated blogPostCreated = new BlogPostCreated(UUID.randomUUID(), "Title", "Body", "Author", Instant.now());
+        BlogPostDeleted blogPostDeleted = new BlogPostDeleted(blogPostCreated.getAggregateId());
+
+        updateService.create(blogPostCreated);
+
+        updateService.delete(blogPostDeleted);
+
+    }
 }
